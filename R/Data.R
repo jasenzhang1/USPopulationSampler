6bb733eb8786d65f7c60eb4faf3b805e05bd1f4a
jasenzhang1 <jaz001@ucsd.edu>
2024-07-08 13:15:29 -0400

    statefp dataset

---
+++ R/Data.R
@@ -0,0 +1,13 @@
+#' United States FIPS numbers for each state
+#'
+#'
+#' @format ## `statefp`
+#' A data frame with 56 rows and 2 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"statefp"
---
+++ R/jasenfirst.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param x A number
+#'
+#' @return The number squared
+#' @export
+#'
+#' @examples
+#' jasenfirst(5)
+#' jasenfirst(3.2)
+jasenfirst <- function(x){
+  return(x^2)
+}
