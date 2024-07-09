9543fdd62f32b6c5daa5917a15f70d20ede9e9b7
jasenzhang1 <jaz001@ucsd.edu>
2024-07-09 10:11:32 -0400

    df_state dataset

--- R/Data.R
+++ R/Data.R
@@ -11,3 +11,17 @@
 #' }
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
 "statefp"
+
+#' Shape files for each state
+#'
+#'
+#' @format ## `df_state`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"df_state"
--- tests/testthat/test-jasenfirst.R
+++ tests/testthat/test-jasenfirst.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("simple case", {
+  expect_equal(jasenfirst(5), 25)
+  expect_equal(jasenfirst(-5), 25)
+  expect_equal(jasenfirst(4.2), 17.64)
 })
+
+test_that('no strings', {
+  expect_error(jasenfirst('a'), 'non-numeric argument')
+})
+
+
