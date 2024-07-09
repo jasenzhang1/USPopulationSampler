1e3caf91d86a5b3accaa51338723812f9e5c1093
jasenzhang1 <jaz001@ucsd.edu>
2024-07-09 10:24:55 -0400

    county, tract, bg datasets

--- R/Data.R
+++ R/Data.R
@@ -25,3 +25,45 @@
 #' }
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
 "df_state"
+
+#' Shape files for each county
+#'
+#'
+#' @format ## `county_shapes`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"county_shapes"
+
+#' Shape files for each tract
+#'
+#'
+#' @format ## `tract_shapes`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"tract_shapes"
+
+#' Shape files for each block group
+#'
+#'
+#' @format ## `bg_shapes`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"bg_shapes"
