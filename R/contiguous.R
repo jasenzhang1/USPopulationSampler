31f6dee2676cd496f581c426393cc2d44cb0760f
jasenzhang1 <jaz001@ucsd.edu>
2024-07-09 11:44:44 -0400

    contiguous US filter

---
+++ R/contiguous.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @param df dataframe with STATEFP as a column
+#'
+#' @return a dataframe with just information from the contiguous US
+#' @export
+#'
+#' @examples
+#' df_contiguous <- contiguous(df)
+#'
+contiguous <- function(df){
+  contiguous_state_fp <- c('01', '04', '05', '06')
+  contiguous_state_fp <- c(contiguous_state_fp,
+                           as.character(c(10:13, 16:42, 44:51, 53:56)))
+
+  df2 <- df %>% dplyr::filter(STATEFP %in% contiguous_state_fp)
+
+  return(df2)
+}
