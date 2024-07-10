f7f0dd518135d785da11da992114cafebb59d515
jasenzhang1 <jaz001@ucsd.edu>
2024-07-10 14:23:08 -0700

    shape + pop data at all tiers

--- R/Data.R
+++ R/Data.R
@@ -26,6 +26,48 @@
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
 "df_state"
 
+#' Shape files for each state
+#'
+#'
+#' @format ## `df_county`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"df_county"
+
+#' Shape files for each state
+#'
+#'
+#' @format ## `df_tract`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"df_tract"
+
+#' Shape files for each state
+#'
+#'
+#' @format ## `df_bg`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"df_bg"
+
 #' Shape files for each county
 #'
 #'
@@ -67,3 +109,59 @@
 #' }
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
 "bg_shapes"
+
+#' population for each state
+#'
+#'
+#' @format ## `state_pop`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"state_pop"
+
+#' population for each state
+#'
+#'
+#' @format ## `county_pop`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"county_pop"
+
+#' population for each state
+#'
+#'
+#' @format ## `tract_pop`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"tract_pop"
+
+#' population for each block group
+#'
+#'
+#' @format ## `bg_pop`
+#' A data frame with 56 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"bg_pop"
