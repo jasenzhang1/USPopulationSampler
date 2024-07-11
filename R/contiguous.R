9ca8160ca36a15190909c1c2b985cf0537d8ab9e
jasenzhang1 <jaz001@ucsd.edu>
2024-07-11 10:46:41 -0700

    contiguous function update

--- R/contiguous.R
+++ R/contiguous.R
@@ -9,7 +9,7 @@
 #' df_contiguous <- contiguous(df)
 #'
 contiguous <- function(df){
-  contiguous_state_fp <- c('01', '04', '05', '06')
+  contiguous_state_fp <- c('01', '04', '05', '06', '08', '09')
   contiguous_state_fp <- c(contiguous_state_fp,
                            as.character(c(10:13, 16:42, 44:51, 53:56)))
 
